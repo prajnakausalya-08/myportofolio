1053ed9aab3767af092b66c4a416a2adceab7def
Prajna Kausalya Damdami <prajna.kausalya@ui.ac.id>
2026-09-09 16:18:36 +0700

    feat: implement MVT for profile and experience

---
+++ main/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Prajna Kausalya Damdami",
+        "npm": "2506657213",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "A Computer Science Student who deeply interested in "
+            "technology, programming, design, and arts with high creativity. "
+            "Lately focused on studying in CS Universitas Indonesia."
+        ),
+    }
+
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Prajna Kausalya Damdami",
+        "experience_list": Experience.objects.all(),
+    }
+
+    return render(request, "experience.html", context)
