58154c03f957af1664d55fde3f3748631b3c7d21
Prajna Kausalya Damdami <prajna.kausalya@ui.ac.id>
2026-09-14 21:10:21 +0700

    feat: add education creation route

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, show_experience, show_education
+from main.views import show_main, show_experience, show_education, create_education
 
 app_name = "main"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
     path("education/", show_education, name="show_education"),
+    path("education/add/", create_education, name="create_education"),
 ]
